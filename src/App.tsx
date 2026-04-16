a080f489f9257586bf0b54f05709fca0d4280637
7shi <7shi@live.jp>
2026-04-17 05:33:31 +0900

    Show version number in UI title

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { VideoPlayer } from './components/VideoPlayer';
 import { FileControls } from './components/FileControls';
 import { SubtitleList } from './components/SubtitleList';
+import { version } from '../package.json';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <main className="max-w-5xl mx-auto flex flex-col gap-4">
         <header className="flex items-baseline justify-between">
           <div className="flex items-baseline gap-1">
-            <h1 className="text-2xl font-bold text-gray-800">SRT Edit</h1>
+            <h1 className="text-2xl font-bold text-gray-800">SRT Edit {version}</h1>
             <a
               href="https://github.com/7shi/srt-edit"
               target="_blank"
